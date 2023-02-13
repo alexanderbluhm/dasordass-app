63c3a97880fa525fe64f99b3f4ce7ea7141336ee
Alexander Bluhm <lamiy-develop@live.de>
2023-02-13 16:01:22 +0100

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter as createInter } from "@next/font/google";
 import clsx from "clsx";
+import PlausibleProvider from "next-plausible";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -33,6 +34,9 @@ const inter = createInter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de" className={clsx("bg-brand-50 dark:bg-brand-900 antialiased font-sans text-brand-900 dark:text-white", inter.variable)}>
+      <head>
+        <PlausibleProvider domain="dasoderdass.de" />
+      </head>
       <body className="max-w-3xl mx-auto px-5 py-12">{children}</body>
     </html>
   );
